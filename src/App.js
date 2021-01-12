1d43e86596d6cca9c072605d802217a82f43e6a3
Melinda <melinda.sava.td@gmail.com>
2021-01-12 15:35:39 +0100

    made it work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './HelloWorld';
 import PFTest from './PFTest/index.js';
+import List from './List';
 import { useState } from 'react';
 
 const PEOPLE = [{HWname:'Krille', HWprofession:'Coder Extraordinarie'}];
@@ -11,7 +12,11 @@ function App() {
   const [HWname, setHWname] = useState('Krille');
   const [profession, setProfession] = useState('');
   const [HWprofession, setHWprofession] = useState('Coder Extraordinarie');
-  
+
+  const addPerson = (name, profession) => {
+    PEOPLE.push({name: name, profession: profession, key: name});
+    console.log(PEOPLE);
+  }
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -21,11 +26,13 @@ function App() {
     } else {
       setHWname(name);
       setHWprofession(profession);
+      addPerson(name, profession);
     }
 
     setName('');
     setProfession('');
-    PEOPLE.push({HWname:{HWname}, HWprofession:{HWprofession}});
+    
+    //PEOPLE.push({HWname:HWname, HWprofession:HWprofession, key:HWname});
     
   }
   console.log(PEOPLE);
@@ -36,21 +43,23 @@ function App() {
       {/* <HelloWorld 
         HWname={HWname} 
         HWprofession={HWprofession} /> */}
-      <div>
-        {
-          PEOPLE.map(person => {
-            return (
-            <HelloWorld HWname={person.HWname} HWprofession={person.HWprofession} />
-            )
-          })
-        }
-      </div>
+      <HelloWorld HWname={HWname} HWprofession={HWprofession} />
       <PFTest 
         name={name} 
         setName={setName} 
         profession={profession} 
         setProfession={setProfession} 
         handleSubmit={handleSubmit} />
+      <div>
+        
+        {
+          PEOPLE.map(person => <>
+            <List 
+            name={person.name} 
+            profession={person.profession} />
+            </>)
+        }
+      </div>
       <p className="App">Tjaba!</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/List/index.js
@@ -0,0 +1,10 @@
+function List (props) {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.profession}</p>
+        </div>
+    )
+}
+
+export default List;
